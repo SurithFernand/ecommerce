ceaa50edfa2a57646946dc916817877b0a1f2a1e
SurithFernand <surith444@gmail.com>
2026-03-09 17:44:19 +0530

    Creating the model

--- src/main/java/com/quickcart/ecommerce/QuickCartApplication.java
+++ src/main/java/com/quickcart/ecommerce/QuickCartApplication.java
@@ -1,5 +1,6 @@
 package com.quickcart.ecommerce;
 
+import com.quickcart.ecommerce.model.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,12 @@ public class QuickCartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuickCartApplication.class, args);
+
+		Product espresso = new Product(1, "Espresso", 2.99);
+		Product croissant = new Product(2, "Croissant", 3.49);
+
+		System.out.println("Product ID:" + espresso.getId() + "    Name:" + espresso.getName() + "   Price : $" + espresso.getPrice());
+		System.out.println("Product ID:" + croissant.getId() + "    Name:" + croissant.getName() + "    Price : $" + croissant.getPrice());
 	}
 
 }
