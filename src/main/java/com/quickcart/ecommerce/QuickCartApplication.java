592bde0898c6a7a1dc7c666796b9d02e643055c5
SurithFernand <surith444@gmail.com>
2026-03-10 23:51:28 +0530

    Creating the controller

--- src/main/java/com/quickcart/ecommerce/QuickCartApplication.java
+++ src/main/java/com/quickcart/ecommerce/QuickCartApplication.java
@@ -10,11 +10,11 @@ public class QuickCartApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuickCartApplication.class, args);
 
-		Product espresso = new Product(1, "Espresso", 2.99);
-		Product croissant = new Product(2, "Croissant", 3.49);
-
-		System.out.println("Product ID:" + espresso.getId() + "    Name:" + espresso.getName() + "   Price : $" + espresso.getPrice());
-		System.out.println("Product ID:" + croissant.getId() + "    Name:" + croissant.getName() + "    Price : $" + croissant.getPrice());
+//		Product espresso = new Product(1, "Espresso", 2.99);
+//		Product croissant = new Product(2, "Croissant", 3.49);
+//
+//		System.out.println("Product ID:" + espresso.getId() + "    Name:" + espresso.getName() + "   Price : $" + espresso.getPrice());
+//		System.out.println("Product ID:" + croissant.getId() + "    Name:" + croissant.getName() + "    Price : $" + croissant.getPrice());
 	}
 
 }
